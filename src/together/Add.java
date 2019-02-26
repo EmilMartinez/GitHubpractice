bc6cd8feb475fd93947956bb1b5c7cbc70b77c24
Emil Martinez <emilmrtnz@gmail.com>
2019-02-25 19:37:21 -0500

    Added hello and add

---
+++ src/together/Add.java
@@ -0,0 +1,11 @@
+package together;
+
+public class Add {
+
+    public static void main(String[] args) {
+        int i = 25;
+        int v = 20;
+        int total = i*v;
+
+    }
+}
---
+++ src/together/Hello.java
@@ -0,0 +1,7 @@
+package together;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
